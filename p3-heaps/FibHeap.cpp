8b9493922eb97fef28a15036a8ae645c3e397e2c
Mithul Nallaka <mithul0700@gmail.com>
2021-11-19 17:37:23 -0600

    minor update

--- p3-heaps/FibHeap.cpp
+++ p3-heaps/FibHeap.cpp
@@ -191,7 +191,12 @@ public:
                 d++;
             }
             A[d] = x;
-            w = x->right;
+            if (x->right == x) {
+                w = x->right;
+            } else {
+                w = w->right;
+            }
+
         } while(w != this->head);
 
         this->min = nullptr;
@@ -324,64 +329,121 @@ public:
 };
 
 
-int main() {
+int main() {string A[10] = {"A","B","C","D","E","F","H","I","J","K"};
+    int B[10] = {10,9,8,7,6,5,4,3,2,1};
 
-    // From the FibHeap lab where we did several operations on this set
-    // Feel free to check output with your own hand-drawn trees
+    Heap<int> T1, T2(B,10);
 
-    int ints[] = {6, 2, 10, 7, 5, 1, 11, 9, 3, 4, 8};
-    FibHeap<int> fhInt(ints, 11);
+    T2.printKey();
+    //Should output  1 2 4 3 6 5 8 10 7 9
 
-    // B0
-    // 8
+    for(int i=0; i<10; i++) T1.insert(B[i]);
+
+    T1.printKey();
+    // Should output 1 2 5 4 3 9 6 10 7 8
+
+    cout << T2.peekKey() << endl;
+    //Should output 1
+
+    cout << T1.extractMin() << endl;
+    //Should output 1
+
+    T1.printKey();
+    //Should output 2 3 5 4 8 9 6 10 7
+
+
+    FibHeap<string> X(A,10), Y;
+
+    X.printKey();
+    //Should output
+    //B1
+    //J K
     //
-    // B1
-    // 3 4
+    //B3
+    //A E H I F C D B
+
+    cout << X.extractMin() << endl;
+    //Should output A
+
+    X.printKey();
+    //Should output
+    //B0
+    //B
     //
-    // B3
-    // 1 2 7 10 6 9 11 5
+    //B3
+    //C E H I F J K D
 
-    cout << "8.1" << endl;
-    fhInt.printKey();
-    cout << endl;
+    X.insert("Z");
+    X.insert("A");
+    X.insert("P");
 
-    // B1
-    // 5 8
+    X.printKey();
+    //Should output
+    //B0
+    //B
     //
-    // B3
-    // 2 3 9 11 4 7 10 6
-
-    cout << "8.2" << endl;
-    fhInt.extractMin();
-    fhInt.printKey();
-    cout << endl;
-
-    // B2
-    // 6 7 10 22
+    //B3
+    //C E H I F J K D
     //
-    // B3
-    // 3 5 15 19 8 9 11 4
-
-    cout << "8.3" << endl;
-    fhInt.insert(15);
-    fhInt.insert(19);
-    fhInt.insert(22);
-    fhInt.extractMin();
-    fhInt.printKey();
-    cout << endl;
-
-    // B0
-    // 4
+    //B0
+    //Z
     //
-    // B1
-    // 9 11
+    //B0
+    //A
     //
-    // B3
-    // 5 6 7 10 22 15 19 8
+    //B0
+    //P
 
-    cout << "8.4" << endl;
-    fhInt.extractMin();
-    fhInt.printKey();
-    cout << endl;
+
+
+    Y.insert("M");
+    Y.insert("O");
+    Y.insert("G");
+    Y.insert("N");
+    Y.insert("L");
+    Y.insert("A");
+    Y.extractMin();
+
+    Y.printKey();
+    //Should output
+    //B0
+    //L
+    //
+    //B2
+    //G M O N
+
+    Y.merge(X);
+    cout << Y.peekKey() << endl;
+    //Should output A
+
+    Y.printKey();
+    //Should output
+    //B0
+    //L
+    //
+    //B2
+    //G M O N
+    //
+    //B0
+    //B
+    //
+    //B3
+    //C E H I F J K D
+    //
+    //B0
+    //Z
+    //
+    //B0
+    //A
+    //
+    //B0
+    //P
+
+    Y.extractMin();
+
+    Y.printKey();
+    //Should output
+    //B4
+    //B C E H I F J K D G M O N P Z L
 
 }
